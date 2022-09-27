80f547608c1026488c4ede53c40f306f4213eef1
Tadeo Rivera <xtecuan@protonmail.com>
2022-09-27 14:25:38 -0600

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export REPOSITORY_URL="https://github.com/xtecuan/guia-play-with-docker.git"
+
+mkdir -p $HOME/Projects/httpd
+cd $HOME/Projects
+git clone $REPOSITORY_URL
+ln -s $HOME/Projects/guia-play-with-docker  $HOME/Projects/guia1
+cp $HOME/Projects/guia1/httpd/index.html $HOME/Projects/httpd/
