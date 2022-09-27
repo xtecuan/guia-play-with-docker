9a564e94c877ad33dc65a4b4429cff0b1c1b7bc9
Tadeo Rivera <xtecuan@protonmail.com>
2022-09-27 14:37:23 -0600

    scripts bash

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 export REPOSITORY_URL="https://github.com/xtecuan/guia-play-with-docker.git"
 
+apk add nano zip unzip curl
+
 mkdir -p $HOME/Projects/httpd
 cd $HOME/Projects
 git clone $REPOSITORY_URL
